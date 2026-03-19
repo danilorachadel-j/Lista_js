6514cca172f8d5f761e5cf058cd6d5e9a08ccca1
Danilorachadel-j <danilo_rachadel-j@>
2026-03-19 07:24:32 -0300

    add - adicionei a atividade 3

---
+++ exercicio_2.js
@@ -0,0 +1,11 @@
+
+function ordemRegressiva() {
+let contagem = 10;
+
+while (contagem>-1){
+    console.log(contagem);
+    contagem - 1;
+    delay(1000);
+}
+console.log("Lançamento iniciado!")
+}
