2fcb9a062966b95808987da2ed902cfcfa45b240
Danilorachadel-j <danilo_rachadel-j@>
2026-03-19 09:24:23 -0300

    add - alterei o nome do arquivo 4 e iniciei o arquivo 5

---
+++ script_4.js
@@ -0,0 +1,13 @@
+function ordena (){
+    let numeros = [];
+
+    for (let i = 0; i < 4; i++);{
+        numeros.push(Number(promp("Digite um número: ")));
+
+
+    }
+    numeros.sort ((a,b) => b - a);
+    alert("Ordem decrescente: " + numeros);
+}
+
+ordena();
