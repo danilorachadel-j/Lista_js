1b06e7a76a0a8eb8e8b184f234e19a90b8bff101
Danilorachadel-j <danilo_rachadel-j@>
2026-03-24 09:21:14 -0300

    add - adicionei a atividade 7 e 8 e as finalizei

--- script_7.js
+++ script_7.js
@@ -1,5 +1,36 @@
 function sistemaPreco(){
-
+alert("Escolha as seguintes opções de picolé:/ a chocolate/ b morango c creme/ d manga /e melancia/ f vanilla ice/ g céu azul/ h brownie/ i hawaiano: ");
+let sabor = prompt("Escolha um sabor para  descobrir um preço")
+switch(sabor){
+    case "a":
+    alert("Chocolate, preço R$1,50");
+    break
+    case "b":
+    alert("Morango, preço R$2,50");
+    break
+    case "c": 
+    alert("Creme, preço R$2,50");
+    break
+    case "d":
+    alert("Manga, preço R$3,20");
+    break
+    case "e":
+    alert("Melancia, preço R$3,40");
+    break
+    case "f":
+    alert("Vanilla Ice, preço R$3,00");
+    break
+    case "g":
+    alert("Céu Azul, preço R$3,60");
+    break
+    case "h":
+    alert("Brownie, preço R$4,00");
+    break
+    case "i":
+    alert("Hawaiano, preço R$5,00");
+    default
+:}
 
 }
+sistemaPreco();
 
---
+++ script_8.js
@@ -0,0 +1,8 @@
+function duasVariaveis(){
+    let numero = 20;
+    let numero1 = 10;
+    alert("A diferença é de "+ (numero - numero1));
+    alert("O dobro do primeiro numero somado ao triplo do segundo é de "+ (numero*2) + (numero1*3));
+    alert ("A multiplicação entre eles é de "+ numero*numero1);
+}
+duasVariaveis();
