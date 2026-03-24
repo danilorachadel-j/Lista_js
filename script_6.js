066ea9e18b4d495022de4667a0391aefe39f07a0
Danilorachadel-j <danilo_rachadel-j@>
2026-03-24 08:27:38 -0300

    add - adicionei e finalizei a atividade 06

---
+++ script_6.js
@@ -0,0 +1,13 @@
+function vogalConsoante (){
+let letra = prompt("Digite uma letra: ");
+while(letra)
+if (letra ==='a'|| letra === 'e'|| letra ==='i'|| letra ==='o'|| letra==='u'){
+alert("Vogal: ");
+}else{
+    alert("Consoante: ");
+}
+ 
+}
+vogalConsoante();
+
+
