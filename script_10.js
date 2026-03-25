01449410045622029ba39d955232e95bb515e6d6
Danilorachadel-j <danilo_rachadel-j@>
2026-03-25 07:55:13 -0300

    add - terminei a atividade 10

--- script_10.js
+++ script_10.js
@@ -1,9 +1,14 @@
 function pagamentoSimplificado(){
 let nome = 
-let salario = 0;
+let salario = 1500;
 let desconto = salario*0.08;
 
 alert("Digite seu nome: "+ nome);
-alert("Digite seu salário ")
-}
+alert("Digite seu salário "+ salario);
 
+alert("Seu nome é "+nome)
+alert ("Seu salário é de "+ salario);
+alert("O seu desconto do INSS é de "+ desconto);
+alert ("O seu salário liquido é de "+(salario - desconto));
+}
+pagamentoSimplificado();
